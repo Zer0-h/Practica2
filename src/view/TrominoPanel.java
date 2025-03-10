53317c7192b4284b785066e00501006504077da9
Zer0 <vilojz@gmail.com>
2025-03-10 15:29:33 +0100

    See chessboard while putting square

--- src/view/TrominoPanel.java
+++ src/view/TrominoPanel.java
@@ -10,6 +10,7 @@ public class TrominoPanel extends JPanel {
     private int[][] board;
     private int fixedX = -1, fixedY = -1;
     private boolean tileSelected = false;
+    private boolean solvingStarted = false;
 
     public TrominoPanel(int size) {
         setPreferredSize(new Dimension(600, 600));
@@ -18,25 +19,30 @@ public class TrominoPanel extends JPanel {
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
+                if (solvingStarted) {
+                    return; // Prevent changes after solving
+                }
                 int tileSize = getWidth() / board.length;
                 int x = e.getY() / tileSize;
                 int y = e.getX() / tileSize;
 
-                // Set the missing tile if not already selected
-                if (!tileSelected) {
-                    fixedX = x;
-                    fixedY = y;
-                    board = new int[board.length][board.length]; // Reset board
-                    board[fixedX][fixedY] = -1; // Set clicked tile as fixed
-                    tileSelected = true;
-                    repaint();
+                // Allow changing the missing tile until solving starts
+                if (tileSelected) {
+                    board[fixedX][fixedY] = 0; // Reset previous tile
                 }
+
+                fixedX = x;
+                fixedY = y;
+                board[fixedX][fixedY] = -1; // Set new missing tile
+                tileSelected = true;
+                repaint();
             }
         });
     }
 
     public void updateBoard(int[][] newBoard) {
-        this.board = newBoard;
+        board = newBoard;
+        solvingStarted = true; // Hide initial grid when solving starts
         repaint();
     }
 
@@ -45,6 +51,7 @@ public class TrominoPanel extends JPanel {
         fixedX = -1;
         fixedY = -1;
         tileSelected = false;
+        solvingStarted = false;
         repaint();
     }
 
@@ -60,47 +67,52 @@ public class TrominoPanel extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         int tileSize = getWidth() / board.length;
-        g.setColor(Color.BLACK);
 
-        // Step 1: Draw the fixed tile (black square)
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                int x = j * tileSize;
-                int y = i * tileSize;
-
-                if (board[i][j] == -1) {
-                    g.setColor(Color.BLACK);
-                    g.fillRect(x, y, tileSize, tileSize);
-                    g.setColor(Color.BLACK);
-                    g.drawRect(x, y, tileSize, tileSize);
-                }
+        // Step 1: Draw the initial grid with black borders
+        if (!solvingStarted) {
+            g.setColor(Color.BLACK);
+            for (int i = 0; i <= board.length; i++) {
+                g.drawLine(0, i * tileSize, getWidth(), i * tileSize); // Horizontal lines
+                g.drawLine(i * tileSize, 0, i * tileSize, getHeight()); // Vertical lines
             }
         }
 
-        // Step 2: Draw the trominoes' borders
-        g.setColor(Color.BLACK);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] > 0) { // Part of a tromino
+        // Step 2: Highlight the selected missing tile (before solving)
+        if (!solvingStarted && tileSelected) {
+            int x = fixedY * tileSize;
+            int y = fixedX * tileSize;
+            g.setColor(Color.BLACK);
+            g.fillRect(x, y, tileSize, tileSize);
+        }
+
+        // Step 3: Draw the trominoes after solving
+        if (solvingStarted) {
+            g.setColor(Color.BLACK);
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
                     int x = j * tileSize;
                     int y = i * tileSize;
 
-                    // Draw only the borders of tromino tiles
-                    if (i == 0 || board[i - 1][j] != board[i][j]) // Top border
-                    {
-                        g.drawLine(x, y, x + tileSize, y);
-                    }
-                    if (j == 0 || board[i][j - 1] != board[i][j]) // Left border
-                    {
-                        g.drawLine(x, y, x, y + tileSize);
-                    }
-                    if (i == board.length - 1 || board[i + 1][j] != board[i][j]) // Bottom border
-                    {
-                        g.drawLine(x, y + tileSize, x + tileSize, y + tileSize);
-                    }
-                    if (j == board[i].length - 1 || board[i][j + 1] != board[i][j]) // Right border
-                    {
-                        g.drawLine(x + tileSize, y, x + tileSize, y + tileSize);
+                    if (board[i][j] == -1) {
+                        g.fillRect(x, y, tileSize, tileSize); // Keep the fixed tile black
+                    } else if (board[i][j] > 0) { // Part of a tromino
+                        // Draw only the borders of tromino tiles
+                        if (i == 0 || board[i - 1][j] != board[i][j]) // Top border
+                        {
+                            g.drawLine(x, y, x + tileSize, y);
+                        }
+                        if (j == 0 || board[i][j - 1] != board[i][j]) // Left border
+                        {
+                            g.drawLine(x, y, x, y + tileSize);
+                        }
+                        if (i == board.length - 1 || board[i + 1][j] != board[i][j]) // Bottom border
+                        {
+                            g.drawLine(x, y + tileSize, x + tileSize, y + tileSize);
+                        }
+                        if (j == board[i].length - 1 || board[i][j + 1] != board[i][j]) // Right border
+                        {
+                            g.drawLine(x + tileSize, y, x + tileSize, y + tileSize);
+                        }
                     }
                 }
             }
