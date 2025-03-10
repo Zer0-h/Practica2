48fe67015e98c34bff8a8578686566ee6a63d511
Zer0 <vilojz@gmail.com>
2025-03-10 14:44:30 +0100

    Fix tromino logic

--- src/model/TrominoModel.java
+++ src/model/TrominoModel.java
@@ -1,76 +1,101 @@
 package model;
 
-import java.awt.Color;
-
 public class TrominoModel {
 
     private int[][] board;
     private int boardSize;
     private int fixedX, fixedY;
-    private int trominoCounter = 1;
-    private final Color[] colors = {Color.RED, Color.GREEN, Color.YELLOW, Color.MAGENTA};
+    private int currentNum;
 
+    // Constructor: Initializes board, sets fixed tile
     public TrominoModel(int size, int fixedX, int fixedY) {
-        this.boardSize = size;
+        // Ensure size is a power of 2
+        int actualSize = 1;
+        while (actualSize < size) {
+            actualSize *= 2;
+        }
+        this.boardSize = actualSize;
         this.fixedX = fixedX;
         this.fixedY = fixedY;
-        this.board = new int[size][size];
-        board[fixedX][fixedY] = -1; // Mark fixed tile
+        this.board = new int[boardSize][boardSize];
+        this.currentNum = 1;
+
+        // Fill board with empty values
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                board[i][j] = 0;
+            }
+        }
+
+        // Set the fixed black tile
+        board[fixedX][fixedY] = -1;
     }
 
+    // Wrapper method for recursion
     public void solveTromino() {
-        placeTromino(0, 0);
+        tileRec(boardSize, 0, 0);
     }
 
-    private boolean placeTromino(int row, int col) {
-        if (row >= boardSize) {
-            return true;
+    // Recursive method to fill board with trominoes
+    private void tileRec(int size, int topX, int topY) {
+        if (size == 2) {
+            // Base case: fill a single tromino
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (board[topX + i][topY + j] == 0) {
+                        board[topX + i][topY + j] = currentNum;
+                    }
+                }
+            }
+            currentNum++;
+            return;
         }
 
-        int nextRow = (col + 1) >= boardSize ? row + 1 : row;
-        int nextCol = (col + 1) >= boardSize ? 0 : col + 1;
-
-        if (board[row][col] != 0) {
-            return placeTromino(nextRow, nextCol);
-        }
-
-        // Try placing each L-shaped tromino
-        for (int[][] shape : new int[][][]{
-            {{0, 0}, {0, 1}, {1, 0}}, // L-shape 1
-            {{0, 0}, {0, 1}, {1, 1}}, // L-shape 2
-            {{0, 0}, {1, 0}, {1, 1}}, // L-shape 3
-            {{0, 1}, {1, 0}, {1, 1}} // L-shape 4
-        }) {
-            if (canPlace(row, col, shape)) {
-                for (int[] s : shape) {
-                    board[row + s[0]][col + s[1]] = trominoCounter;
+        // Find the quadrant with the missing tile
+        int saveX = topX, saveY = topY;
+        for (int x = topX; x < topX + size; x++) {
+            for (int y = topY; y < topY + size; y++) {
+                if (board[x][y] != 0) {
+                    saveX = x;
+                    saveY = y;
                 }
-                trominoCounter++;
-
-                if (placeTromino(nextRow, nextCol)) {
-                    return true;
-                }
-
-                // Backtrack
-                for (int[] s : shape) {
-                    board[row + s[0]][col + s[1]] = 0;
-                }
-                trominoCounter--;
             }
         }
 
-        return false;
-    }
+        // Identify which quadrant contains the missing tile
+        boolean inUpperLeft = saveX < topX + size / 2 && saveY < topY + size / 2;
+        boolean inUpperRight = saveX < topX + size / 2 && saveY >= topY + size / 2;
+        boolean inBottomLeft = saveX >= topX + size / 2 && saveY < topY + size / 2;
+        boolean inBottomRight = saveX >= topX + size / 2 && saveY >= topY + size / 2;
 
-    private boolean canPlace(int row, int col, int[][] shape) {
-        for (int[] s : shape) {
-            int r = row + s[0];
-            int c = col + s[1];
-            if (r >= boardSize || c >= boardSize || board[r][c] != 0) {
-                return false;
-            }
+        // Place a tromino in the center
+        int centerX = topX + size / 2 - 1;
+        int centerY = topY + size / 2 - 1;
+        board[centerX][centerY + 1] = currentNum;
+        board[centerX + 1][centerY] = currentNum;
+        board[centerX + 1][centerY + 1] = currentNum;
+
+        // Mark missing tile in each quadrant
+        if (!inUpperLeft) {
+            board[centerX][centerY] = currentNum;
         }
-        return true;
+        if (!inUpperRight) {
+            board[centerX][centerY + 1] = currentNum;
+        }
+        if (!inBottomLeft) {
+            board[centerX + 1][centerY] = currentNum;
+        }
+        if (!inBottomRight) {
+            board[centerX + 1][centerY + 1] = currentNum;
+        }
+
+        currentNum++;
+
+        // Recursive calls for each quadrant
+        tileRec(size / 2, topX, topY); // Upper Left
+        tileRec(size / 2, topX, topY + size / 2); // Upper Right
+        tileRec(size / 2, topX + size / 2, topY); // Bottom Left
+        tileRec(size / 2, topX + size / 2, topY + size / 2); // Bottom Right
     }
 
     public int[][] getBoard() {
@@ -79,7 +104,7 @@ public class TrominoModel {
 
     public void clearBoard() {
         board = new int[boardSize][boardSize];
-        board[fixedX][fixedY] = -1; // Reset fixed tile
-        trominoCounter = 1;
+        board[fixedX][fixedY] = -1; // Keep fixed tile
+        currentNum = 1;
     }
 }
